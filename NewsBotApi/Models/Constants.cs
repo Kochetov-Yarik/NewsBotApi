6f50d2297f23167bbe515ac8383cf0edbdc91da4
Kochetov-Yarik <bogbana9@gmail.com>
2026-05-21 09:48:43 +0300

    Update Constants.cs
    Security change

--- NewsBotApi/Models/Constants.cs
+++ NewsBotApi/Models/Constants.cs
@@ -2,10 +2,10 @@
 {
     public class Constants
     {
-        public const string RapidApiKey = "d5e2130dc9msha841dc32eb1393bp174731jsnf433dec9501d";
+        public const string RapidApiKey = "Ключ для RapidApi";
 
-        public const string DiskordToken = "MTUwNTQ5NDIyOTkxOTU5NjYxNQ.GU0i5f.9CcXhMS1f0bomXy5ayIja_4gNP6cPCnCyGe9hw";
+        public const string DiskordToken = "Токен підключення до Discord-бота";
 
-        public const string DatabaseConnection = "Host=localhost;Port=5432;Database=NewsBotDB;Username=postgres;Password=xE9jRd60";
+        public const string DatabaseConnection = "Підключення до бази даних";
     }
 }
